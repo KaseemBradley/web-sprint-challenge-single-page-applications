b3cb2cc97b957d90db329e6ab6f248c7fc143bc8
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-23 13:31:25 -0400

    added dropdown for sizes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Home from './components/Home';
-import OrderForm from './components/OrderForm';
-import { Route } from 'react-router-dom';
+import Home from "./components/Home";
+import OrderForm from "./components/OrderForm";
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Route exact path='/' component={Home} />
-      <Route path='/pizza' component={OrderForm}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/pizza" component={OrderForm} />
     </>
   );
 };
--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,10 +1,19 @@
-import React from 'react'; 
+import React from "react";
 
 export default function OrderForm() {
-
-    return (
-        <div>
-            <input type='text'></input>
-        </div>
-    )
+  return (
+    <form id="pizza-form">
+      <label>
+        Name
+        <input id="name-input" name="name" />
+      </label>
+      <br />
+      <select id="size-dropdown">
+        <option>--Size?--</option>
+        <option> Small </option>
+        <option> Med </option>
+        <option> Large </option>
+      </select>
+    </form>
+  );
 }
