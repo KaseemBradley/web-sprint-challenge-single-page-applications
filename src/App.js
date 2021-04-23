04107bcac3baaf82c6aa6cb0de8f1d7dee4b208b
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-23 10:52:15 -0400

    rendered Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Home from './components/Home';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Route path='/' component={Home} />
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    
+    return (
+        <div>
+            <h1>Kaseem's Pizza</h1>
+            <button id="order-pizza">Order</button>
+        </div>
+    )
+}
