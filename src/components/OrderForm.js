0d660572e6530a232521ae9ee2155274eceaa610
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-23 14:05:35 -0400

    added checkbox inputs to form

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -7,13 +7,34 @@ export default function OrderForm() {
         Name
         <input id="name-input" name="name" />
       </label>
+
       <br />
+
       <select id="size-dropdown">
         <option>--Size?--</option>
         <option> Small </option>
         <option> Med </option>
         <option> Large </option>
       </select>
+
+      <p>Toppings</p>
+
+      <label>
+        Onions
+        <input type="checkbox" name="onions" />
+      </label>
+      <label>
+        Pineapple
+        <input type="checkbox" name="pineapple" />
+      </label>
+      <label>
+        Beef
+        <input type="checkbox" name="beef" />
+      </label>
+      <label>
+        Mushrooms
+        <input type="checkbox" name="mushrooms" />
+      </label>
     </form>
   );
 }
